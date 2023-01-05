b77926a2912ef23d4b51d35099424c6aa1a333ef
Liul-Sintayehu <liulsintayehu7@gmail.com>
2023-01-04 21:22:59 -0800

    database is added

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const express = require('express')
+
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const router = require('./Router');
+const cors = require('cors')
 
 const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 app.listen(3001,()=>{
     console.log('on port 3001');
 })
-app.get('/',(req,res)=>{
-    res.send({
-        msg:'eyoba endet neh'
-    })
+const uri = 'mongodb+srv://liul:eKwJXBaSPD6sHSCX@cluster0.up98o7c.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(uri,()=>{
+    console.log('connected to atlas db');
 })
+
+app.use(router)
